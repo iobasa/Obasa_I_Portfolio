98fef2ecfad24de6a790aecfb2a9483cfcaed33a
iobasa <i_obasa@fanshaweonline.ca>
2019-12-12 13:49:35 -0500

    adding changes to the lightboc and functionality

--- config/mailcreds.js
+++ config/mailcreds.js
@@ -1,7 +1,7 @@
 const mailcreds = {
     service: 'gmail',
-    user: 'ifeobasa12@gmail.com', // your email (gmail) goes here, don't use mine
-    pass: 'Loveland1809'// see above
+    user: 'ifeobasa16@gmail.com', // your email (gmail) goes here, don't use mine
+    pass: 'IfeK1234'// see above
 }
 
 module.exports = mailcreds;
